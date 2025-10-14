60e88a09b180c888396086b564ec757f67f805f9
KethraPackiamB <kethrapackiamb@gmail.com>
2025-10-14 11:31:38 +0530

    Created navbar and header content

---
+++ src/BasicHtmlWebsite/Header/HeaderContent.tsx
@@ -0,0 +1,11 @@
+export const HeaderContent = () => {
+
+    return(
+        <div>
+            <h1 className="mb-4">Frontend Developer</h1>
+            <p>👋 Hello! I’m a Frontend Developer passionate about building visually appealing, high-performance, and user-friendly websites.
+                Turning creative designs into interactive and responsive digital experiences is what I do best.
+            </p>
+        </div>
+    )
+}
